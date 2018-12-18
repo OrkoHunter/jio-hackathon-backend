65f67d6c02280d231cae1a56771a775b9d09bf11
Himanshu Mishra <himanshu2014iit@gmail.com>
2018-12-18 21:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,26 +11,17 @@ Fetch static data
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-def create_app():
-    app = Flask(__name__, static_url_path='/static')
-    sess = Session()
-    app.config['SESSION_TYPE'] = 'filesystem'
 
-    # sess.init_app(app)
+app = Flask(__name__, static_url_path='/static')
+sess = Session()
+app.config['SESSION_TYPE'] = 'filesystem'
+# sess.init_app(app)
 
-    define_routes(app)
-    return app
-
-
-def define_routes(app):
-    # Valid routes
-    @app.route("/")
-    def main():
-        return "Hello"
-
-
+# Valid routes
+@app.route("/")
+def main():
+    return "Hello"
 
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000)
