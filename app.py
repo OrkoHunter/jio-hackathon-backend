a29d01e737a57b6d9713ec73de4cf5f17d2e16f6
Himanshu Mishra <himanshu2014iit@gmail.com>
2018-12-18 22:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def verify_facebook():
     VERIFY_TOKEN = "1234553asdcds3"
 
     mode = request.args.get("hub.mode")
-    token = request.args.get("hub.token")
+    token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
 
     if mode and token:
