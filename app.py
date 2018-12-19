046f00d9942324aef0f404eb18dea7af4acca8a2
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-12-19 16:11:45 +0530

    Adds wait time

--- app.py
+++ app.py
@@ -229,12 +229,11 @@ def handleMessage(psid, msg) :
                 sending_sender_action(psid, 'typing_on')
                 #Send results 
                 endpoint = "http://disection.herokuapp.com/disease_check"
-                sample_image_url = "https://raw.githubusercontent.com/spMohanty/PlantVillage-Dataset/master/raw/color/Apple___healthy/011d02f3-5c3c-4484-a384-b1a0a0dbdec1___RS_HL%207544.JPG"
-
-                r = requests.post(endpoint, {"image_url": sample_image_url})
+                r = requests.post(endpoint, {"image_url": attachmentUrl})
                 
                 sending_sender_action(psid, 'typing_off')
-                callSendAPI(psid, r.content)
+                print(r.content)
+                callSendAPI(psid,{"text" :r.content } )
         elif msg["attachments"][0]["type"] == "audio" :
             attachmentUrl = msg["attachments"][0]["payload"]["url"]
             callSendAPI(psid,{"text" : "Got your audio. Please wait till I process it."})
